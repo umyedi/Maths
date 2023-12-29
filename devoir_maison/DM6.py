5dc04f904f306b63890674995476ecf80cfff585
zzz <timoleroux2006@gmail.com>
2023-12-29 18:04:51 +0100

    Correction DM6

--- devoir_maison/DM6.py
+++ devoir_maison/DM6.py
@@ -1,47 +1,34 @@
-import itertools
-from typing import List
-
-
-def encode(a: int, b: int, list_x: List[int]) -> List:
-    """
-    Encode une liste de valeurs en utilisant le chiffrement affine avec les coefficients 'a' et 'b'.
-
-    Args:
-        a (int): Coefficient multiplicatif, entier naturel compris entre 0 (inclus) et 26 (exclus).
-        b (int): Coefficient additif, entier naturel compris entre 0 (inclus) et 26 (exclus).
-        list_x (List[int]): Liste des valeurs originales à encoder (liste d'entiers relatifs).
-
-    Returns:
-        List: Liste des valeurs encodées correspondant à chaque élément de 'list_x'.
-
-    Exemple:
-        >>> encode(3, 1, [0, 1, 2])
-        [1, 4, 7]
-    """
-    return [(a * x + b) % 26 for x in list_x]
-
-
-def decode(list_x: List, list_y: List) -> List:
-    """
-    Décrypte une liste de valeurs encodées en utilisant le chiffrement affine et retourne les coefficients de décodage.
-
-    Args:
-        list_x (List): Liste des indices des caractères avant encodage (liste d'entiers relatifs).
-        list_y (List): Liste des indices des caractères après encodage (liste d'entiers relatifs).
-
-    Returns:
-        List: La paire (a, b) utilisée pour l'encodage si une solution existe, sinon None.
-
-    Exemple:
-        >>> decode([0, 1, 2], [1, 4, 7])
-        (3, 1)
-    """
-    for a, b in itertools.product(range(1, 26), range(26)):
-        if all(y == (a * x + b) % 26 for x, y in zip(list_x, list_y)):
-            return (a, b)
+def modular_inverse(number: int, modulo: int) -> int:
+    for inverse in range(modulo):
+        if (number * inverse) % modulo == 1:
+            return inverse
     return None
 
-list_x = list(range(26)) # Liste des entiers de 0 à 25 inclus
-list_y = encode(3, 11, list_x) # Liste encodé
 
-print(decode(list_x, list_y)) # Trouve les entiers a et b avec lesquelles la liste 'list_x' a été encodé
+def encode_number_affine(a: int, b: int, x: int, modulo: int) -> int:
+    return (a * x + b) % modulo
+
+
+def decode_number_affine(a: int, b: int, y: int, modulo: int) -> int:
+    if modular_inverse(a, modulo) is None:
+        return None
+    mod_inv = modular_inverse(a, modulo)
+    return (y - b) * mod_inv % modulo
+
+def display_table(a: int, b: int):
+    letters = [chr(i + 65) for i in range(26)]
+    indices = list(range(26))
+    encoded_indices = [encode_number_affine(a, b, i, 26) for i in indices]
+    encoded_letters = [chr(i + 65) for i in encoded_indices]
+
+    print("-" * 106)
+    print("|" + "|".join(f"{char:^3}" for char in letters) + "|")
+    print("-" * 106)
+    print("|" + "|".join(f"{i:^3}" for i in indices) + "|")
+    print("-" * 106)
+    print("|" + "|".join(f"{i:^3}" for i in encoded_indices) + "|")
+    print("-" * 106)
+    print("|" + "|".join(f"{char:^3}" for char in encoded_letters) + "|")
+    print("-" * 106)
+
+display_table(3, 11)
